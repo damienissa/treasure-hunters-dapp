ed6f9a361ffc6cf2b8594c591db14c5f945c00c2
Dmytro Virych <inf@dmytrovirych.com>
2025-01-04 21:57:13 +0200

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import WebApp from '@twa-dev/sdk'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
+WebApp.ready();
 
 // Test comment
 createRoot(document.getElementById('root')!).render(
