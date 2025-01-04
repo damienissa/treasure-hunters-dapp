efd8211f0081451185aee5e1ca7d1fa52b8b9d6f
Dmytro Virych <inf@dmytrovirych.com>
2025-01-04 23:34:28 +0200

    Update styles and localization files
    - Updated styles in `src/App.css`
    - Modified main application component in `src/App.tsx`
    - Updated English localization in `src/locales/en.json`
    - Updated Ukrainian localization in `src/locales/uk.json`

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import './App.css';
-
-import React from 'react';
+import WebApp from '@twa-dev/sdk';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import './App.css';
 
 const App: React.FC = () => {
   const { t } = useTranslation();
+  const [userName, setUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Retrieve the user's first name from the WebApp SDK
+    const user = WebApp.initDataUnsafe?.user;
+    setUserName(user?.first_name || 'Guest'); // Default to 'Guest' if no name is available
+  }, []);
 
   return (
     <div className="App">
       {/* Localized Button */}
-      <button
-        className="image-button"
-      >
-        {t('wallet')}
-      </button>
+      <button className="image-button">{t('wallet')}</button>
       <div className="text-container">
-
         <p
           className="description"
-          dangerouslySetInnerHTML={{ __html: t('welcome_message') }}
+          dangerouslySetInnerHTML={{
+            __html: t('welcome_message', { userName }),
+          }}
         ></p>
         <div className="character-image" />
       </div>
