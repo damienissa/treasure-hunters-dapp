5a538e82530b2b0c2bcbc368467353fc4cbd27d2
Dmytro Virych <inf@dmytrovirych.com>
2025-01-04 21:25:22 +0200

    Initial

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+  build: {
+    outDir: './docs'
+  },
+  base: './'
+});
