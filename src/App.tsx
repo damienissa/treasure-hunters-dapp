9fc2065f94a6a9ca8d272fdea71c3c4601f568bb
Dmytro Virych <inf@dmytrovirych.com>
2025-01-04 22:25:41 +0200

    Refactor App component and update styles; replace button with SVG image button

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,14 @@
-import { useState } from 'react'
 import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
-import WebApp from '@twa-dev/sdk'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      {/* Here we add our button with alert callback */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-          Show Alert
-        </button>
-      </div>
-    </>
+    <div className="button-image-container">
+      <button className="image-button" onClick={() => alert('Button Clicked!')}>
+
+      </button>
+    </div>
   )
 }
 
